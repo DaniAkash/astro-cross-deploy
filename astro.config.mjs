395986cf2bb50e45e6f8ad0dd98916226d34cc48
DaniAkash <s.daniakash@gmail.com>
2023-01-02 17:00:11 +0530

    feat: added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
+import ci from 'ci-info';
 
 // https://astro.build/config
 import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
+import vercel from '@astrojs/vercel/edge';
+
+const adapter = ci.NETLIFY ? netlify() : ci.VERCEL ? vercel(): undefined
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter,
 });
